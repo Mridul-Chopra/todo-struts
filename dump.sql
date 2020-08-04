9bef6a7be2af1489bd5d2983ee2edb50f0374c0f
Mridul-Chopra <59679184+Mridul-Chopra@users.noreply.github.com>
2020-08-04 18:34:58 +0530

    Add files via upload

---
+++ dump.sql
@@ -0,0 +1,57 @@
+/*
+SQLyog Community v11.25 (64 bit)
+MySQL - 5.1.42-community : Database - todoStruts
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`todoStruts` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `todoStruts`;
+
+/*Table structure for table `todos` */
+
+DROP TABLE IF EXISTS `todos`;
+
+CREATE TABLE `todos` (
+  `todo_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `todo` varchar(40) DEFAULT NULL,
+  `isDone` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`todo_id`),
+  KEY `fk` (`user_id`),
+  CONSTRAINT `fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+/*Data for the table `todos` */
+
+insert  into `todos`(`todo_id`,`user_id`,`todo`,`isDone`) values (2,1,'test',1),(3,1,'test',0),(4,1,'test',0);
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) DEFAULT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`email`,`username`,`password`) values (1,'mridulchopra97@gmail.com','Mridul Chopra','123'),(2,'mridul.chopra@paxcel.net','user','123');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
